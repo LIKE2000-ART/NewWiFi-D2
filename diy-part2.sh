3619b1cdcd503e1790b518a3b9b704813bd28ef6
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2021-09-02 10:49:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,13 +11,15 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
-# 删除老argon
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+# 删除老argon luci-app-jd-dailybonus
 rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-app-jd-dailybonus
+
 # 拉取argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 拉取微信推送、京东签到插件
 git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
-#git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
